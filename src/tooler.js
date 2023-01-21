29689dc660e79789e4ccf189cdde9ab88a484944
Radicc <tomas.radic00@gmail.com>
2023-01-21 14:53:30 +0100

    updateFooterUI

--- src/tooler.js
+++ src/tooler.js
@@ -1,89 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import HTMLlogo from "../src/images/HTML5logo.png";
+import CSSlogo from "../src/images/CSS3logo.png";
+import SASSlogo from "../src/images/SASSlogo.png";
+import JSlogo from "../src/images/JSlogo.png";
+import TSlogo from "../src/images/TSlogo.png";
+import REACTlogo from "../src/images/REACTlogo.png";
+import WEBPACKlogo from "../src/images/WEBPACKlogo.png";
+import GITlogo from "../src/images/GITlogo.png";
+
 export default function Tooler() {
   return (
     <div className="toolsBar">
-      <img
-        className="toolsLogo"
-        src="https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-logo-0.png"
-      />
+      <img className="toolsLogo" src={HTMLlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png"
-      />
+      <img className="toolsLogo" src={CSSlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png"
-      />
+      <img className="toolsLogo" src={SASSlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png"
-      />
+      <img className="toolsLogo" src={JSlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://cdn.worldvectorlogo.com/logos/typescript-2.svg"
-      />
+      <img className="toolsLogo" src={TSlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
-      />
+      <img className="toolsLogo" src={REACTlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://raw.githubusercontent.com/webpack/media/master/logo/icon-square-big.png"
-      />
+      <img className="toolsLogo" src={WEBPACKlogo} />
 
-      <img
-        className="toolsLogo"
-        src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
-      />
+      <img className="toolsLogo" src={GITlogo} />
       <div className="div1">
-        <img
-          className="toolsLogo2"
-          src="https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-html-logo-0.png"
-        />
+        <img className="toolsLogo2" src={HTMLlogo} />
 
-        <img
-          className="toolsLogo2"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png"
-        />
+        <img className="toolsLogo2" src={CSSlogo} />
 
-        <img
-          className="toolsLogo2"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png"
-        />
+        <img className="toolsLogo2" src={SASSlogo} />
 
-        <img
-          className="toolsLogo2"
-          src="https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png"
-        />
+        <img className="toolsLogo2" src={JSlogo} />
       </div>
       <div className="div2">
-        <img
-          className="toolsLogo2"
-          src="https://cdn.worldvectorlogo.com/logos/typescript-2.svg"
-        />
+        <img className="toolsLogo2" src={TSlogo} />
 
-        <img
-          className="toolsLogo2"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
-        />
+        <img className="toolsLogo2" src={REACTlogo} />
 
-        <img
-          className="toolsLogo2"
-          src="https://raw.githubusercontent.com/webpack/media/master/logo/icon-square-big.png"
-        />
+        <img className="toolsLogo2" src={WEBPACKlogo} />
 
-        <img
-          className="toolsLogo2"
-          src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
-        />
+        <img className="toolsLogo2" src={GITlogo} />
       </div>
     </div>
   );
