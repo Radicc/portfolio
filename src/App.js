c406fa26a28327dd7942f66c754f1c1ad26a91d9
Radicc <tomas.radic00@gmail.com>
2023-01-28 21:09:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ function App() {
             </div>
             <h1 className="tomRadText">A Frontend developer</h1>
             <h3 className="tomRadUnderText">
-              I am a self-taught student of frontend developing trying to change
-              profession. Experienced in HTML,CSS and JavaScript specializing in
-              ReactJS library. Implementing CSS-extansion SASS and
-              JavaScript-syntactical superset TypeScript.
+              I am a self-taught student of frontend development trying to
+              change profession. Experienced in HTML, CSS and JavaScript
+              specializing in ReactJS library. Implementing CSS-extension SASS
+              and JavaScript-syntactical superset TypeScript.
             </h3>
           </div>
 
