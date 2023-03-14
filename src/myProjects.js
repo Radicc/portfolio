f9b3d6b5f83c3088e2f9b571b5ce177033e3bbda
Radicc <tomas.radic00@gmail.com>
2023-03-14 18:38:37 +0100

    update

--- src/myProjects.js
+++ src/myProjects.js
@@ -6,53 +6,20 @@ import ProjectFuture from "./images/ProjectNow.jpg";
 import WallStreetProject from "./images/wallstreetProject.jpg";
 import Portfolio from "./images/Portfolio.jpg";
 
+import htmlLogo from "./images/html.png";
+import sassLogo from "./images/sass.png";
+import cssLogo from "./images/css.png";
+import javaScriptLogo from "./images/javascript.png";
+import reactLogo from "./images/react.png";
+import npmLogo from "./images/npm.png";
+
 export default function MyProjects() {
   return (
     <div id="Projects" className="myProjectsBody">
       <h1 className="projectsText">Projects</h1>
       <div className="projectBox1">
         <img
-          onClick={() =>
-            window.open("https://github.com/Radicc/profile", "_blank")
-          }
           className="imgBox1"
-          src={Portfolio}
-          alt="error"
-        ></img>
-        <div className="projectContainer">
-          <h2 className="projectContainerTitul">Portfolio</h2>
-          <p className="textBox">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed
-            turpis in urna lacinia luctus. Phasellus placerat tempor urna nec
-            condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et
-            ultrices posuere cubilia curae; Fusce dignissim tempus vestibulum.
-            Aenean sed imperdiet ex. Etiam nisi eros, consectetur in venenatis
-            nec, congue fermentum augue. Aenean tristique viverra metus, vel
-            suscipit diam egestas ut. Nam scelerisque ipsum in lacinia placerat.
-            Nullam id posuere massa, sit amet rhoncus nulla. Morbi suscipit urna
-            vitae tellus viverra, sed scelerisque odio ultrices.
-          </p>
-          <div className="CVbutton2Container">
-            <div
-              disabled
-              onClick={() =>
-                window.open("https://github.com/Radicc/profile", "_blank")
-              }
-              className="Cvbutton2"
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              Code
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="projectBox2">
-        <img
-          className="imgBox2"
           src={WallStreetProject}
           onClick={() =>
             window.open("https://radicc.github.io/Research_Platform/", "_blank")
@@ -60,10 +27,18 @@ export default function MyProjects() {
         ></img>
         <div className="projectContainer2">
           <h2 className="projectContainerTitul">Research Platform</h2>
+          <div className="logoes">
+            <img src={htmlLogo} />
+            <img src={sassLogo} />
+            <img src={javaScriptLogo} />
+            <img src={reactLogo} />
+            <img src={npmLogo} />
+          </div>
           <p className="textBox">
             Free investment educative website for people that want to improve
-            money management skills. Retirement calculator and study sections
-            with the best investment books, youtube channels and websites.
+            money management skills. Retirement calculator, API stock news and
+            study sections with important investment books, youtube channels and
+            websites.
           </p>
 
           <div className="CVbutton2Container">
@@ -97,6 +72,53 @@ export default function MyProjects() {
               <span></span>
               <span></span>
               VISIT
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="projectBox2">
+        <img
+          onClick={() =>
+            window.open("https://github.com/Radicc/profile", "_blank")
+          }
+          className="imgBox2"
+          src={Portfolio}
+          alt="error"
+        ></img>
+        <div className="projectContainer">
+          <h2 className="projectContainerTitul">Portfolio</h2>
+          <p className="textBox">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed
+            turpis in urna lacinia luctus. Phasellus placerat tempor urna nec
+            condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia curae; Fusce dignissim tempus vestibulum.
+            Aenean sed imperdiet ex. Etiam nisi eros, consectetur in venenatis
+            nec, congue fermentum augue. Aenean tristique viverra metus, vel
+            suscipit diam egestas ut. Nam scelerisque ipsum in lacinia placerat.
+            Nullam id posuere massa, sit amet rhoncus nulla. Morbi suscipit urna
+            vitae tellus viverra, sed scelerisque odio ultrices.
+          </p>
+          <div className="logoes">
+            <img src={htmlLogo} />
+            <img src={cssLogo} />
+            <img src={javaScriptLogo} />
+            <img src={reactLogo} />
+            <img src={npmLogo} />
+          </div>
+          <div className="CVbutton2Container">
+            <div
+              disabled
+              onClick={() =>
+                window.open("https://github.com/Radicc/profile", "_blank")
+              }
+              className="Cvbutton2"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Code
             </div>
           </div>
         </div>
